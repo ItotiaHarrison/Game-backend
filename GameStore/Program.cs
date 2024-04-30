6e5a5ce115252e0cfa8a7b1662d17624679d694b
Harrison Itotia <harrisonitotia@gmail.com>
2024-04-30 17:08:16 +0300

    Implement data transfer objects

--- GameStore/Program.cs
+++ GameStore/Program.cs
@@ -1,5 +1,15 @@
+using GameStore.Dtos;
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
+
+List<GameDto> games = [
+    new (1, "Super Mario Bros", "Platformer", 59.99m, new DateOnly(1985, 9, 13)),
+    new (2, "The Legend of Zelda", "Action-adventure", 59.99m, new DateOnly(1986, 2, 21)),
+    new (3, "Minecraft", "Sandbox", 26.95m, new DateOnly(2011, 11, 18))
+];
+
+app.MapGet("games",  ()=> games);
 
 app.MapGet("/", () => "Hello World!");
 
