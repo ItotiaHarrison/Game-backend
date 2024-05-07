ea90ad13621e51e924bf5a6ede33c846e82f2b80
Harrison Itotia <harrisonitotia@gmail.com>
2024-05-07 22:24:24 +0300

    Seeding data

--- GameStore/obj/Debug/net8.0/GameStore.AssemblyInfo.cs
+++ GameStore/obj/Debug/net8.0/GameStore.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GameStore")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aa0df02ad4e145c0f2bfb79f6eba297815798b5d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2eb503dda95dbdfc0dba3ccd355e666887acecd6")]
 [assembly: System.Reflection.AssemblyProductAttribute("GameStore")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GameStore")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
